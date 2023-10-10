02f97797f694e99c11d8636977826a5eeeecc66e
Connor Adams <connorvadams@gmail.com>
2023-10-09 19:34:47 -0600

    Initial commit.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import { Segment, Image } from "semantic-ui-react";
 import * as Images from "../services/Images";
+import HostInfo from './HostInfo'
 
 function Details() {
   // We'll render the logo if no host is selected. But if a host does get selected....
   // Watch the video to see how this works in the app.
+  const [hostSelected, setHostSelected] = useState(false)
 
   return (
     <Segment id="details" className="HQComps">
-      {true ? <Image size="medium" src={Images.westworldLogo} /> : null}
+      {!hostSelected ? <Image size="medium" src={Images.westworldLogo} /> : <HostInfo />}
     </Segment>
   );
 }
--- src/components/WestworldMap.js
+++ src/components/WestworldMap.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
+import Host from './Host'
 
 function WestworldMap() {
   return <Segment id="map">{/* What should we render on the map? */}</Segment>;
